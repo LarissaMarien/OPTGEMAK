27fed48a96f6285be8308a7e9dfa9afaab885a17
Larissa Marien <larissa.marien@student.ap.be>
2019-11-14 15:49:11 +0100

    iconen op kaart aangepast + detailpagina gemaakt

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,5 @@
 var map = L.map('map', {
-  center: [51.210325, 4.658203],
-  zoom: 15
+  center: [51.210325, 4.658203]
 });
 
 var basicmap = L.tileLayer('https://tile.thunderforest.com/mobile-atlas/{z}/{x}/{y}.png?apikey=202db2b8cca0453498c2139dc0591c9d', {
@@ -10,7 +9,13 @@ var basicmap = L.tileLayer('https://tile.thunderforest.com/mobile-atlas/{z}/{x}/
 function onLocationFound(e) {
 	var radius = e.accuracy;
 
-	L.marker(e.latlng).addTo(map)
+  var toiletpapier = L.icon({
+      iconUrl: '/images/logo_blauw.png',
+      iconSize:     [30, 30],
+      popupAnchor:  [-3, -8]
+    });
+
+	L.marker(e.latlng, {icon: toiletpapier}).addTo(map)
 		.bindPopup("Je bent hier!").openPopup();
 
   L.circle(e.latlng).setRadius(500).addTo(map).bindPopup("De cirkel heeft een radius van 1KM");
@@ -20,4 +25,4 @@ map.on('locationfound', onLocationFound);
 
 basicmap.addTo(map);
 
-map.locate({setView: true, maxZoom: 14});
+map.locate({setView: true, maxZoom: 15});
