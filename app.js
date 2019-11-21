f8c10b76688a26206aab8e7cf2aff1c5842172c6
Larissa Marien <larissa.marien@student.ap.be>
2019-11-21 09:46:04 +0100

    Final touches + aanmaken contact pagina + style

--- app.js
+++ app.js
@@ -54,6 +54,10 @@ app.get('/toiletdetail/:toiletid', function(req, res){
   });
 });
 
+app.get('/contact', function(req, res){
+  res.render('contact');
+});
+
 request('https://geodata.antwerpen.be/arcgissql/rest/services/P_Portal/portal_publiek1/MapServer/8/query?where=1%3D1&outFields=OBJECTID,ID,OBDD,CATEGORIE,PUBLICEREN,PRIORITAIR,OMSCHRIJVING,EXTRA_INFO_PUBLIEK,VRIJSTAAND,TYPE,STADSEIGENDOM,BETALEND,STRAAT,HUISNUMMER,POSTCODE,DISTRICT,BEHEERDER,CONTACTPERSOON,CONTACTGEGEVENS,VERMELDING,DOELGROEP,INTEGRAAL_TOEGANKELIJK,GESCREEND,LUIERTAFEL,OPENINGSUREN_OPM,OPM_INTERN,LAT,LONG,X_COORD,Y_COORD,SHAPE&outSR=4326&f=json',
   function(error, response, body){
     data_toiletten = JSON.parse(body);
--- public/js/lijst.js
+++ public/js/lijst.js
@@ -10,9 +10,9 @@ var iso = new Isotope( '.grid', {
 var filtersElem = document.querySelector('.filters-button-group');
 filtersElem.addEventListener( 'click', function( event ) {
   // only work with buttons
-  if ( !matchesSelector( event.target, 'button' ) ) {
+  /*if ( !matchesSelector( event.target, 'button' ) ) {
     return;
-  }
+  }*/
   var filterValue = event.target.getAttribute('data-filter');
   // use matching filter function
   //filterValue = filterFns[ filterValue ] || filterValue;
